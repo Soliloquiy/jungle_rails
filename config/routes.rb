400c060f1c71a29c065f7e60e96b5bef6c3cd22f
Soliloquy <ibrahia@ucalgary.ca>
2021-07-22 15:00:21 +0000

    Add feature user authentication and edit nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,16 @@ Rails.application.routes.draw do
     resources :categories
   end
 
+
+  #Users resource (registration)
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  #Sessions resource (login)
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
