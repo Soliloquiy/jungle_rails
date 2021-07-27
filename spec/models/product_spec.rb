72b88e1fae9680723d9901ab60e83de827045b81
Soliloquy <ibrahia@ucalgary.ca>
2021-07-27 04:54:54 +0000

    Add test for product validations

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,95 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it "is valid with valid attributes of name, price, quantity and category" do
+      @category = Category.new
+      @category.id = 4
+      @category.name = 'Technology'
+      @category.save!
+
+      @product = Product.new
+      @product.name = "phone"
+      @product.price = 20.00
+      @product.quantity = 5
+      @product.category_id = @category.id
+      @product.save!
+
+      @category.products = [@product]
+      
+
+      expect(@category.products[0].name).to be_present
+      expect(@category.products[0].price).to be_present
+      expect(@category.products[0].quantity).to be_present
+      expect(@category.products[0].category_id).to be_present
+    end
+    
+
+    it "is not valid without a name" do
+      @category = Category.new
+      @category.id = 4
+      @category.name = 'Technology'
+      @category.save!
+
+      @product = Product.new
+      @product.name = nil
+      @product.price_cents = 2000
+      @product.quantity = 5
+      @product.category_id = @category.id
+      @product.save
+      
+      expect(@product).not_to be_valid
+    end
+
+    it "is not valid without a price" do
+      @category = Category.new
+      @category.id = 4
+      @category.name = 'Technology'
+      @category.save!
+
+      @product = Product.new
+      @product.name = "Phone"
+      @product.price_cents = nil
+      @product.quantity = 5
+      @product.category_id = @category.id
+      @product.save
+      
+      expect(@product).not_to be_valid
+    end
+
+    it "is not valid without a quantity" do
+      @category = Category.new
+      @category.id = 4
+      @category.name = 'Technology'
+      @category.save!
+
+      @product = Product.new
+      @product.name = "Phone"
+      @product.price_cents = 2000
+      @product.quantity = nil
+      @product.category_id = @category.id
+      @product.save
+      
+      expect(@product).not_to be_valid
+    end
+
+    it "is not valid without a category_id" do
+      @category = Category.new
+      @category.id = 4
+      @category.name = 'Technology'
+      @category.save!
+
+      @product = Product.new
+      @product.name = "Phone"
+      @product.price_cents = 2000
+      @product.quantity = nil
+      @product.category_id = nil
+      @product.save
+      
+      expect(@product).not_to be_valid
+    end
+
+  end
+
+
+end
